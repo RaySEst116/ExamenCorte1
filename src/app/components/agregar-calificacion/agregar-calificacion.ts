aec255b10771f173d4a898581362de52b0956732
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2026-02-10 07:26:09 -0700

    [ADD] Recuperacion de datos en el form y edita solamente el registro seleccionado

--- src/app/components/agregar-calificacion/agregar-calificacion.ts
+++ src/app/components/agregar-calificacion/agregar-calificacion.ts
@@ -1,6 +1,14 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
 import { Validators, FormGroup, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { Output } from '@angular/core';
+
+interface Registro {
+  matricula: string;
+  nombre: string;
+  corte1: string;
+  corte2: string;
+  corte3: string;
+}
 
 @Component({
   selector: 'app-agregar-calificacion',
@@ -12,7 +20,8 @@ import { Output } from '@angular/core';
 })
 export class AgregarCalificacion {
   formularioCalif: FormGroup;
-  @Output() registro = new EventEmitter()
+  @Output() registro = new EventEmitter<Registro>()
+  modoEdicion: boolean = false;
 
   constructor(private fb: FormBuilder) {
     this.formularioCalif = this.fb.group({
@@ -24,14 +33,31 @@ export class AgregarCalificacion {
     });
   }
 
+  cargarDatosEdicion(registro: Registro) {
+    this.modoEdicion = true;
+    this.formularioCalif.patchValue({
+      nombre: registro.nombre,
+      matricula: registro.matricula,
+      corte1: registro.corte1,
+      corte2: registro.corte2,
+      corte3: registro.corte3
+    });
+  }
+
   submit() {
     if (this.formularioCalif.valid) {
       this.registro.emit(this.formularioCalif.value)
       this.formularioCalif.reset()
+      this.modoEdicion = false;
 
       console.log(this.formularioCalif.value)
     } else {
       alert('Datos no validos')
     }
+  }
+
+  resetForm() {
+    this.formularioCalif.reset();
+    this.modoEdicion = false;
   }
 }
--- src/app/pages/calificaciones/calificaciones.ts
+++ src/app/pages/calificaciones/calificaciones.ts
@@ -1,5 +1,5 @@
-import { Component, signal, Input } from '@angular/core';
-import { AgregarCalificacion, formularioCalif } from '../../components/agregar-calificacion/agregar-calificacion';
+import { Component, signal, Input, ViewChild } from '@angular/core';
+import { AgregarCalificacion } from '../../components/agregar-calificacion/agregar-calificacion';
 
 interface Registro {
   matricula: string;
@@ -11,7 +11,7 @@ interface Registro {
 
 @Component({
   selector: 'app-calificaciones',
-  imports: [AgregarCalificacion, formularioCalif],
+  imports: [AgregarCalificacion],
   templateUrl: './calificaciones.html',
   styleUrl: './calificaciones.scss',
 })
@@ -20,20 +20,27 @@ export class Calificaciones {
   protected readonly title = signal('examen');
 
   @Input() registros: Registro[] = []
-  @Input() editarRegistro: Registro
+  @ViewChild(AgregarCalificacion) agregarCalificacionComponent!: AgregarCalificacion;
+  registroSeleccionado: Registro | null = null;
+  indiceEdicion: number = -1;
 
-  addRegistro(registro: Registro) { this.registros.push(registro) }
-
-  delete(registro: Registro) {
-    this.registros = this.registros.filter(r => r !== registro) 
+  addRegistro(registro: Registro) {
+    if (this.registroSeleccionado && this.indiceEdicion !== -1) {
+      this.registros[this.indiceEdicion] = registro;
+      this.registroSeleccionado = null;
+      this.indiceEdicion = -1;
+    } else {
+      this.registros.push(registro);
+    }
   }
 
-  edit() {
-    this.formularioCalif.setValue({
-      nombre: this.editarRegistro.nombre,
-      corte1: this.editarRegistro.corte1,
-      corte2: this.editarRegistro.corte2,
-      corte3: this.editarRegistro.corte3
-    })
+  delete(registro: Registro) {
+    this.registros = this.registros.filter(r => r !== registro)
+  }
+
+  edit(registro: Registro) {
+    this.registroSeleccionado = registro;
+    this.indiceEdicion = this.registros.findIndex(r => r.matricula === registro.matricula);
+    this.agregarCalificacionComponent.cargarDatosEdicion(registro);
   }
 }
